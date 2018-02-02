a648ba9bb1472a86b84b7707dd2fce9530ab9ecf
Kathy Mai <kathymai@Kathys-MacBook-Air.local>
2018-02-01 23:36:05 -0800

    updated server.js articlejs and notejs

---
+++ models/note.js
@@ -0,0 +1,20 @@
+// Require mongoose ****
+var mongoose = require("mongoose");
+// Create a schema class
+var Schema = mongoose.Schema;
+
+// Create the Note schema
+var NoteSchema = new Schema({
+  title: {
+    type: String
+  },
+  body: {
+    type: String
+  }
+});
+
+// Create the Note model with the NoteSchema
+var Note = mongoose.model("Note", NoteSchema);
+
+// Export the Note model
+module.exports = Note;
