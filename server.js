e95c742f8acad15f5bcf3acce1c5762a438c0294
Kathy Mai <kathymai@Kathys-MacBook-Air.local>
2018-02-02 00:02:46 -0800

    fixed typo

--- server.js
+++ server.js
@@ -108,7 +108,7 @@ app.get("/scrape", function (req, res) {
     // Route for grabbing a specific Article by id, populate it with it's note
     app.get("/articles/:id", function (req, res) {
         // Grab every doc in the Articles array
-        Article.find({}, function (error, doc) {
+        db.Article.find({}, function (error, doc) {
             // Log any errors
             if (error) {
                 console.log(error);
@@ -126,9 +126,6 @@ app.get("/scrape", function (req, res) {
         // Create a new note and pass the req.body to the entry
         db.Note.create(req.body)
             .then(function (dbNote) {
-                // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-                // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-                // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
                 return db.article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
             })
             .then(function (dbArticle) {
